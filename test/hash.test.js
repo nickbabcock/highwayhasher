d7b2b1f94c6ed3f4c63c9c7b82b01828aecb93e8
Nick Babcock <nbabcock19@hotmail.com>
2020-12-31 07:05:51 -0600

    Parameterize test across both impl and wasm hashers
    This will give confidence that the Wasm hasher also passes all tests on
    node

--- test/hash.test.js
+++ test/hash.test.js
@@ -44,180 +44,180 @@ const keyData = Uint8Array.from([
   31,
 ]);
 
-describe("hash", () => {
-  it("choose correct hash", async () => {
-    if (isNode()) {
-      expect(HighwayHash.name).toEqual("NativeHighwayHash");
-    } else {
-      expect(HighwayHash.name).toEqual("WasmHighwayHash");
-    }
-  });
-
-  it("load and create hash", async () => {
-    const mod = await HighwayHash.loadModule();
-    const hash = mod.create();
-    let out = hash.finalize64();
-    let expected = Uint8Array.from([105, 68, 213, 185, 117, 218, 53, 112]);
-    expect(out).toEqual(expected);
-  });
-
-  it("keyless and dataless 64bit", async () => {
-    const hash = await HighwayHash.load();
-    let out = hash.finalize64();
-    let expected = Uint8Array.from([105, 68, 213, 185, 117, 218, 53, 112]);
-    expect(out).toEqual(expected);
-  });
-
-  it("dataless 64bit", async () => {
-    const hash = await HighwayHash.load(keyData);
-    let out = hash.finalize64();
-    let expected = Uint8Array.from([83, 110, 194, 34, 222, 86, 122, 144]);
-    expect(out).toEqual(expected);
-  });
-
-  it("dataless 128bit", async () => {
-    const hash = await HighwayHash.load(keyData);
-    let out = hash.finalize128();
-    let expected = Uint8Array.from([
-      199,
-      254,
-      143,
-      157,
-      143,
-      38,
-      237,
-      15,
-      111,
-      62,
-      9,
-      127,
-      118,
-      94,
-      86,
-      51,
-    ]);
-    expect(out).toEqual(expected);
-  });
-
-  it("dataless 256bit", async () => {
-    const hash = await HighwayHash.load(keyData);
-    let out = hash.finalize256();
-    let expected = Uint8Array.from([
-      245,
-      116,
-      200,
-      194,
-      42,
-      72,
-      68,
-      221,
-      31,
-      53,
-      199,
-      19,
-      115,
-      1,
-      70,
-      217,
-      255,
-      20,
-      135,
-      185,
-      204,
-      190,
-      174,
-      179,
-      244,
-      29,
-      117,
-      69,
-      49,
-      35,
-      218,
-      65,
-    ]);
-    expect(out).toEqual(expected);
-  });
-
-  it("64bit", async () => {
-    const hash = await HighwayHash.load(keyData);
-    hash.append(Uint8Array.from([0]));
-    let out = hash.finalize64();
-    let expected = Uint8Array.from([120, 221, 205, 199, 170, 67, 171, 126]);
-    expect(out).toEqual(expected);
-  });
-
-  it("64bit wasm", async () => {
-    const hash = await WasmHighwayHash.load(keyData);
-    hash.append(Uint8Array.from([0]));
-    let out = hash.finalize64();
-    let expected = Uint8Array.from([120, 221, 205, 199, 170, 67, 171, 126]);
-    expect(out).toEqual(expected);
-  });
-
-  it("128bit", async () => {
-    const hash = await HighwayHash.load(keyData);
-    hash.append(Uint8Array.from([0]));
-    let out = hash.finalize128();
-    let expected = Uint8Array.from([
-      168,
-      231,
-      129,
-      54,
-      137,
-      168,
-      176,
-      214,
-      180,
-      220,
-      156,
-      235,
-      249,
-      29,
-      41,
-      220,
-    ]);
-    expect(out).toEqual(expected);
-  });
-
-  it("256bit", async () => {
-    const hash = await HighwayHash.load(keyData);
-    hash.append(Uint8Array.from([0]));
-    let out = hash.finalize256();
-    let expected = Uint8Array.from([
-      84,
-      130,
-      95,
-      228,
-      188,
-      65,
-      185,
-      237,
-      15,
-      198,
-      202,
-      61,
-      239,
-      68,
-      13,
-      226,
-      71,
-      74,
-      50,
-      203,
-      155,
-      27,
-      101,
-      114,
-      132,
-      228,
-      117,
-      178,
-      76,
-      98,
-      115,
-      32,
-    ]);
-    expect(out).toEqual(expected);
-  });
+it("choose hash implementation depending on platform", () => {
+  if (isNode()) {
+    expect(HighwayHash.name).toEqual("NativeHighwayHash");
+  } else {
+    expect(HighwayHash.name).toEqual("WasmHighwayHash");
+  }
 });
+
+// Can't use `describe.each` as that is not implemented by karmatic
+const parameters = [
+  ["impl", HighwayHash],
+  ["wasm", WasmHighwayHash],
+];
+
+for (const [name, Hash] of parameters) {
+  describe(`${name} hash`, () => {
+    it("load and create hash", async () => {
+      const mod = await Hash.loadModule();
+      const hash = mod.create();
+      let out = hash.finalize64();
+      let expected = Uint8Array.from([105, 68, 213, 185, 117, 218, 53, 112]);
+      expect(out).toEqual(expected);
+    });
+
+    it("keyless and dataless 64bit", async () => {
+      const hash = await Hash.load();
+      let out = hash.finalize64();
+      let expected = Uint8Array.from([105, 68, 213, 185, 117, 218, 53, 112]);
+      expect(out).toEqual(expected);
+    });
+
+    it("dataless 64bit", async () => {
+      const hash = await Hash.load(keyData);
+      let out = hash.finalize64();
+      let expected = Uint8Array.from([83, 110, 194, 34, 222, 86, 122, 144]);
+      expect(out).toEqual(expected);
+    });
+
+    it("dataless 128bit", async () => {
+      const hash = await Hash.load(keyData);
+      let out = hash.finalize128();
+      let expected = Uint8Array.from([
+        199,
+        254,
+        143,
+        157,
+        143,
+        38,
+        237,
+        15,
+        111,
+        62,
+        9,
+        127,
+        118,
+        94,
+        86,
+        51,
+      ]);
+      expect(out).toEqual(expected);
+    });
+
+    it("dataless 256bit", async () => {
+      const hash = await Hash.load(keyData);
+      let out = hash.finalize256();
+      let expected = Uint8Array.from([
+        245,
+        116,
+        200,
+        194,
+        42,
+        72,
+        68,
+        221,
+        31,
+        53,
+        199,
+        19,
+        115,
+        1,
+        70,
+        217,
+        255,
+        20,
+        135,
+        185,
+        204,
+        190,
+        174,
+        179,
+        244,
+        29,
+        117,
+        69,
+        49,
+        35,
+        218,
+        65,
+      ]);
+      expect(out).toEqual(expected);
+    });
+
+    it("64bit", async () => {
+      const hash = await Hash.load(keyData);
+      hash.append(Uint8Array.from([0]));
+      let out = hash.finalize64();
+      let expected = Uint8Array.from([120, 221, 205, 199, 170, 67, 171, 126]);
+      expect(out).toEqual(expected);
+    });
+
+    it("128bit", async () => {
+      const hash = await Hash.load(keyData);
+      hash.append(Uint8Array.from([0]));
+      let out = hash.finalize128();
+      let expected = Uint8Array.from([
+        168,
+        231,
+        129,
+        54,
+        137,
+        168,
+        176,
+        214,
+        180,
+        220,
+        156,
+        235,
+        249,
+        29,
+        41,
+        220,
+      ]);
+      expect(out).toEqual(expected);
+    });
+
+    it("256bit", async () => {
+      const hash = await Hash.load(keyData);
+      hash.append(Uint8Array.from([0]));
+      let out = hash.finalize256();
+      let expected = Uint8Array.from([
+        84,
+        130,
+        95,
+        228,
+        188,
+        65,
+        185,
+        237,
+        15,
+        198,
+        202,
+        61,
+        239,
+        68,
+        13,
+        226,
+        71,
+        74,
+        50,
+        203,
+        155,
+        27,
+        101,
+        114,
+        132,
+        228,
+        117,
+        178,
+        76,
+        98,
+        115,
+        32,
+      ]);
+      expect(out).toEqual(expected);
+    });
+  });
+}

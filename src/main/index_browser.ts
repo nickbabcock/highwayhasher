b5a6ee397d8fec9ea49a4ee1452b7411bf4c3375
Nick Babcock <nbabcock19@hotmail.com>
2021-12-13 17:57:33 -0600

    Rename web module to wasm
    To more accurately reflect that the module is used to output wasm -- not
    necessarily run on the web (it can run in node js just fine)

--- src/main/index_browser.ts
+++ src/main/index_browser.ts
@@ -1,3 +1,3 @@
 export type { IHash, HashCreator, HighwayLoadOptions } from "./model";
-export { WasmHighwayHash, hasSimd as hasWasmSimd } from "./web";
-export { WasmHighwayHash as HighwayHash } from "./web";
+export { WasmHighwayHash, hasSimd as hasWasmSimd } from "./wasm";
+export { WasmHighwayHash as HighwayHash } from "./wasm";
